3dd92e99430c92d796765098447e7616f0606f70
Kaustav Nag <ikaustav017@gmail.com>
2016-09-13 22:49:16 -0400

    colored text and blank spaces

--- app.js
+++ app.js
@@ -8,18 +8,22 @@ MsgController.$inject = ['$scope'];
 function MsgController($scope) {
   $scope.lunchItems = "";
   $scope.diplayMessage = "";
+  $scope.myStyle ="";
 
   $scope.displayAptMessage = function () {
 
     if($scope.lunchItems==""){
       $scope.diplayMessage ="Please enter data first";
+      $scope.myStyle={color:'red'};
       return;
     }
     var noOfitems = numOfItems($scope.lunchItems);
     if(noOfitems<=3){
       $scope.diplayMessage ="Enjoy!";
+      $scope.myStyle={color:'green'};
     }else if(noOfitems>3) {
       $scope.diplayMessage ="Too much!";
+      $scope.myStyle={color:'green'};
     }
   };
 
@@ -27,9 +31,18 @@ function MsgController($scope) {
     //console.log(items)
     var trimmedItems = items.trim();
     var elements = trimmedItems.split(',');
+    var size =0;
+      //console.log(elements);
+     // console.log(elements.length);
+      for(var i=0; i<elements.length;i++){
 
-      console.log(elements);
-    return elements.length;
+        if( !(elements[i].trim()==="")){
+          // console.log(elements[i]);
+          size+=1;
+        }
+      }
+       console.log(size);
+    return  size; 
 
   };
 }
